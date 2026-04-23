571c5723830a1f49caacdf19984ad86a223bd832
Samuel Chait <chaits@mail.gvsu.edu>
2026-04-23 12:04:11 -0400

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth }       from 'firebase/auth';
+import { getFirestore }  from 'firebase/firestore';
+import { getAnalytics }  from 'firebase/analytics';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWxtBR8T3RNfm-ICRrnTZ4Gk7F_76c0KQ",
-  authDomain: "golfmate-53cd9.firebaseapp.com",
-  projectId: "golfmate-53cd9",
-  storageBucket: "golfmate-53cd9.firebasestorage.app",
+  apiKey:            "AIzaSyBWxtBR8T3RNfm-ICRrnTZ4Gk7F_76c0KQ",
+  authDomain:        "golfmate-53cd9.firebaseapp.com",
+  projectId:         "golfmate-53cd9",
+  storageBucket:     "golfmate-53cd9.firebasestorage.app",
   messagingSenderId: "786778862846",
-  appId: "1:786778862846:web:70f3d545344c915cb421af",
-  measurementId: "G-KWQ8QQ9FCH"
+  appId:             "1:786778862846:web:70f3d545344c915cb421af",
+  measurementId:     "G-KWQ8QQ9FCH"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app       = initializeApp(firebaseConfig);
+export const auth      = getAuth(app);
+export const db        = getFirestore(app);
+export const analytics = getAnalytics(app);
